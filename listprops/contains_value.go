6a6d49b3eab5b10b9e7eaa9f61c33141db4cfbb3
kimlia.knopf@heidelberg.com <kimlia.knopf@heidelberg.com>
2025-11-06 14:42:41 +0100

    listdrop

--- listprops/contains_value.go
+++ listprops/contains_value.go
@@ -3,6 +3,11 @@ package listprops
 // ContainsValue gibt erwartet eine Liste von Zahlen und eine Zahl v.
 // Liefert true, falls v in der Liste enthalten ist, sonst false.
 func ContainsValue(list []int, v int) bool {
-	// TODO
+	for i := 0; i < len(list); i++ {
+		if list[i] == v {
+			return true
+		}
+	}
 	return false
+
 }
--- listprops/count_value.go
+++ listprops/count_value.go
@@ -3,6 +3,6 @@ package listprops
 // CountValue gibt erwartet eine Liste von Zahlen und eine Zahl v.
 // Liefert die Anzahl der Vorkommen von v in der Liste.
 func CountValue(list []int, v int) int {
-	// TODO
+
 	return 0
 }
