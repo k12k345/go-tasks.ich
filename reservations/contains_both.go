8ba9ac15188d08c57bf1502e6aa4046f3770a4dd
kimlia.knopf@heidelberg.com <kimlia.knopf@heidelberg.com>
2025-11-22 14:03:35 +0100

    table

--- reservations/contains_both.go
+++ reservations/contains_both.go
@@ -3,6 +3,16 @@ package reservations
 // ContainsBoth erwartet eine Liste von Strings und zwei Strings s1 und s2.
 // Liefert true, falls sowohl s1 als auch s2 in der Liste enthalten sind, sonst false.
 func ContainsBoth(list []string, s1 string, s2 string) bool {
-	// TODO
+
+	for i := 0; i < len(list); i++ {
+		if list[i] == s1 {
+			for j := 0; j < len(list); j++ {
+				if list[j] == s2 {
+					return true
+				}
+			}
+
+		}
+	}
 	return false
 }
--- reservations/contains_in_order.go
+++ reservations/contains_in_order.go
@@ -3,6 +3,23 @@ package reservations
 // ContainsInOrder erwartet eine Liste von Strings und zwei Strings s1 und s2.
 // Liefert true, falls sowohl s1 als auch s2 in der Liste enthalten sind, und s1 vor s2 kommt.
 func ContainsInOrder(list []string, s1 string, s2 string) bool {
-	// TODO
-	return false
+
+	foundS1 := false
+	foundS2 := false
+	pos1 := 0
+	pos2 := 0
+	for i := 0; i < len(list); i++ {
+		if list[i] == s1 {
+			foundS1 = true
+			pos1 = i
+		}
+		for j := 0; j < len(list); j++ {
+			if list[j] == s2 {
+				foundS2 = true
+				pos2 = j
+			}
+		}
+	}
+	return foundS1 && foundS2 && pos1 < pos2
+
 }
